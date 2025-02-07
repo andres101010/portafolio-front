f66047372e87e84de528491cc0207ee862c278e2
andres@h2d.dev <andreschapu5295@gmail.com>
2025-02-07 18:32:53 -0400

    last changed

--- rutas/proyectos/Proyectos.jsx
+++ rutas/proyectos/Proyectos.jsx
@@ -1,5 +1,6 @@
 import './Proyectos.css'
 import { Grid, Typography } from "@mui/material";
+import Carousel from 'react-material-ui-carousel';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -10,13 +11,31 @@ import loginImg from '/src/assets/img/login.png'
 import pPt from '/src/assets/img/piedra-papel-tijeras.png'
 import tenis from '/src/assets/img/tenis.png'
 import consultora from '/src/assets/img/consultora.png'
+// import mercado1 from '/src/assets/img/mercados1.jpg'
+// import mercado2 from '/src/assets/img/mercados2.jpg'
 
 import { NavLink } from "react-router-dom";
 const Proyecto = ()=>{
+
+  const images = [
+    { src: '/src/assets/img/mercados1.jpg', title: 'Mercado 1' },
+    { src: '/src/assets/img/mercados2.jpg', title: 'Mercado 2' },
+    { src: '/src/assets/img/mercados3.jpg', title: 'Mercado 3' },
+    { src: '/src/assets/img/mercados4.jpg', title: 'Mercado 4' },
+    { src: '/src/assets/img/mercados5.jpg', title: 'Mercado 5' },
+    { src: '/src/assets/img/mercados6.jpg', title: 'Mercado 6' },
+    { src: '/src/assets/img/mercados7.jpg', title: 'Mercado 7' }
+  ];
+  const images2 = [
+    { src: '/src/assets/img/location1.png', title: 'location 1' },
+    { src: '/src/assets/img/location2.png', title: 'location 2' },
+    { src: '/src/assets/img/location3.png', title: 'location 3' },
+    
+  ];
     return(
         <Grid container m={"auto"} >
             <Grid item md={12} xs={12} color={'green'} textAlign={'center'} mb={'10px'}>
-            <Typography variant="h2">Proyectos</Typography>
+            <Typography variant="h2">Proyectos Iniciales</Typography>
             </Grid>
 
     <Grid item md={4} m={'auto'} mb={'5px'} className="itemProyect">
@@ -150,6 +169,128 @@ const Proyecto = ()=>{
       </CardActions>
     </Card>
      </Grid>
+
+     {/* PROYECTOS AVANZADOS */}
+
+     <Grid item md={12} xs={12} color={'green'} textAlign={'center'} mb={'10px'}>
+            <Typography variant="h2">Proyectos Avanzados</Typography>
+     </Grid>
+
+     {/* carrusel */}
+
+     <Grid item md={7} m={'auto'} mb={'5px'} className="itemProyect">
+      <Card sx={{ width: 800 }} className="itemProyect">
+
+        {/* 🖼 Carousel con imágenes */}
+        <Carousel>
+          {images.map((img, index) => (
+            <CardMedia
+              key={index}
+              sx={{ height: 390, width: 'auto' }}
+              image={img.src}
+              title={img.title}
+            />
+          ))}
+        </Carousel>
+
+        {/* 📌 Contenido de la Card */}
+        <CardContent style={{ backgroundColor: 'black', color: 'white' }}>
+          <Typography gutterBottom variant="h5" component="div">
+            Sistema Para El Control De Pago De Alquiler Para Mercados
+          </Typography>
+          <Typography variant="body2" color="white">
+          He desarrollado una plataforma integral para la gestión eficiente de mercados y arrendatarios. Con este sistema, los administradores pueden optimizar el cobro de rentas y mejorar el control financiero de los puestos de mercado.
+          🔹 Funcionalidades principales:
+          ✅ Inicio de sesión y gestión de usuarios 👥
+          ✅ Registro y administración de mercados y arrendatarios 📋
+          ✅ Pagos digitales de los puestos asignados 💳
+          ✅ Generación de actas de entrega 📄
+          ✅ Historial de pagos y observaciones 🔍
+          ✅ Gráficos de estadísticas para análisis financiero 📊
+          💡 Tecnología utilizada:
+          Desarrollado con Node.js, Express, React.js y MongoDB, garantizando un sistema escalable, rápido y seguro.
+          
+          </Typography>
+          <Typography variant="body2" color="white">Credenciales:</Typography>
+          <Typography variant="body2" color="white">admin@gmail.com</Typography>
+          <Typography variant="body2" color="white">1234</Typography>
+        </CardContent>
+
+        {/* 🔗 Enlace */}
+        <CardActions style={{ backgroundColor: 'black', color: 'white' }}>
+          Link:
+          <NavLink 
+            to="https://mercados-front.vercel.app/" 
+            target="_blank" 
+            style={{ textDecoration: 'none', marginLeft: "55px", color: 'blue' }}>
+            https://mercados-front.vercel.app/
+          </NavLink>
+        </CardActions>
+
+      </Card>
+    </Grid>
+
+    <Grid item md={7} m={'auto'} mb={'5px'} mt={5} className="itemProyect">
+      <Card sx={{ width: 800 }} className="itemProyect">
+
+        {/* 🖼 Carousel con imágenes */}
+        <Carousel>
+          {images2.map((img, index) => (
+            <CardMedia
+              key={index}
+              sx={{ height: 390, width: 'auto' }}
+              image={img.src}
+              title={img.title}
+            />
+          ))}
+        </Carousel>
+
+        {/* 📌 Contenido de la Card */}
+        <CardContent style={{ backgroundColor: 'black', color: 'white' }}>
+          <Typography gutterBottom variant="h5" component="div">
+          🌍 Proyecto de Localización y Moneda
+          </Typography>
+          <Typography variant="body2" color="white">
+            🚀 Tecnologías Utilizadas
+            Backend: Node.js + Express
+            Frontend: React.js
+            Cliente HTTP: Axios
+            APIs Consumidas:
+            vatcomply.com → Para obtener información de monedas.
+            ipinfo.io / restcountries.com → Para geolocalización y datos del país.
+                      
+          </Typography>
+          <Typography variant="body2" color="white"> 
+            🎯 Objetivo del Proyecto
+          El proyecto permite obtener automáticamente la moneda local del usuario con base en su dirección IP. Se utiliza una combinación de APIs para determinar el país y luego consultar la moneda correspondiente.
+          </Typography>
+          <Typography variant="body2" color="white">
+          🔧 Funcionamiento
+            Obtener la dirección IP del usuario.
+            Se obtiene la IP desde el backend usando req.ip o una API externa.
+            Consultar la API de geolocalización.
+            Se usa una API similar para obtener el país del usuario.
+            Determinar la moneda del país.
+            Se consulta restcountries.com para obtener el código y símbolo de la moneda.
+            Mostrar la moneda en la interfaz de usuario.
+            El frontend en React muestra la información en tiempo real. Al igual que guarda el registro de las monedas buscadas en una base de datos MySql
+          </Typography>
+          
+        </CardContent>
+
+        {/* 🔗 Enlace */}
+        <CardActions style={{ backgroundColor: 'black', color: 'white' }}>
+          Link:
+          <NavLink 
+            to="https://lnkd.in/eFmQeSW6" 
+            target="_blank" 
+            style={{ textDecoration: 'none', marginLeft: "55px", color: 'blue' }}>
+            https://lnkd.in/eFmQeSW6
+          </NavLink>
+        </CardActions>
+
+      </Card>
+    </Grid>
 
     </Grid>
     )
